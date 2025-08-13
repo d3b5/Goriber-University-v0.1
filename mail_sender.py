eb3d640ae1f129dbcac56219434ac53ef137b7c9
deb <debaleenasarker2004@gmail.com>
2025-08-13 22:38:20 +0600

    contact issue

--- mail_sender.py
+++ mail_sender.py
@@ -1,14 +1,13 @@
-import smtplib,ssl,os
-from dotenv import load_dotenv
+import smtplib, ssl, os
 
 def send_mail(message):
     host = "smtp.gmail.com"
     port = 465
     sender_email = "parkmejihyo@gmail.com"
-    password = os.getenv("PASSWORD")
-    reciever_email = sender_email
+    password = os.environ["PASSWORD"]  # will fetch from Streamlit secrets
+    receiver_email = sender_email
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, reciever_email, message)
+        server.sendmail(sender_email, receiver_email, message)
